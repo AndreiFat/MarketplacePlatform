4f624d8e840a14e169c92b7e3cc489e3ef8d5603
Andrei <fatandrei57@gmail.com>
2023-12-13 16:39:54 +0200

    Initial commit

---
+++ src/test/java/com/project/marketplaceplatform/MarketplacePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.marketplaceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplacePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

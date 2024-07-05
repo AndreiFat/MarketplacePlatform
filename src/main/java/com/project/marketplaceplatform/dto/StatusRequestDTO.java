d0fd4c1198e2e169d2c311256c945ebfcb2fa5e5
Andrei <fatandrei57@gmail.com>
2024-07-05 21:29:30 +0300

    Bug Fixes

--- src/main/java/com/project/marketplaceplatform/dto/StatusRequestDTO.java
+++ src/main/java/com/project/marketplaceplatform/dto/StatusRequestDTO.java
@@ -1,10 +1,22 @@
 package com.project.marketplaceplatform.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class StatusRequestDTO {
     private List<OrderStatus> statusHistory;
+
+    public StatusRequestDTO() {
+        this.statusHistory = new ArrayList<>();  // Initialize the list here
+    }
+
+    public void addStatus(OrderStatus status) {
+        this.statusHistory.add(status);
+    }
 }
+

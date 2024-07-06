4ee6662f62446266115827c1933b71f07072ffb2
Andrei <fatandrei57@gmail.com>
2024-07-06 15:37:42 +0300

    Small Bugs

--- src/react-frontend/src/Layouts/AdminLayout.jsx
+++ src/react-frontend/src/Layouts/AdminLayout.jsx
@@ -1,14 +1,14 @@
 import {Container} from "react-bootstrap";
 import {Outlet} from "react-router-dom";
-import AdminHeader from "../components/AdminHeader.jsx";
 import Footer from "../components/Footer.jsx";
 import RouteVerifier from "../components/RouteVerifier.jsx";
+import Header from "../components/Header.jsx";
 
 function GeneralLayout() {
     return (
         <>
             <Container fluid className={"p-0 mt-5"}>
-                <AdminHeader/>
+                <Header/>
                 <Container>
                     <Outlet/>
                 </Container>
